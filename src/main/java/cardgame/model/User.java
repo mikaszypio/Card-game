ab57563519781c072e0c79334116ebba12d74653
mikaszypio <szypiorowski.m@gmail.com>
2018-12-25 18:22:11 +0100

    Update user model

--- src/main/java/cardgame/model/User.java
+++ src/main/java/cardgame/model/User.java
@@ -2,7 +2,10 @@ package cardgame.model;
 
 public class User {
 
+	private int userId;
 	private String username;
+	private String password;
+	private int score;
 	
 	public User() {
 		
@@ -12,11 +15,40 @@ public class User {
 		this.username = username;
 	}
 	
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+	
 	public String getUsername() {
 		return username;
 	}
 	
+	public String getPassword() {
+		return password;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
 	public void setUsername(String username) {
 		this.username = username;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
 	}
 }
