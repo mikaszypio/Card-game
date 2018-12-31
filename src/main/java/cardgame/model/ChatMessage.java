cdad7764178ffa0815b570dbc00f1845c0b73d3f
mikaszypio <szypiorowski.m@gmail.com>
2018-12-31 16:05:06 +0100

    Update ChatMessage model

--- src/main/java/cardgame/model/ChatMessage.java
+++ src/main/java/cardgame/model/ChatMessage.java
@@ -2,17 +2,33 @@ package cardgame.model;
 
 public class ChatMessage {
 
+	private String author;
 	private String content;
+	private String time;
 	
 	public ChatMessage() {
 		
 	}
 	
 	public ChatMessage(String content) {
+		this(null, content, null);
+	}
+	
+	public ChatMessage(String author, String content, String time) {
+		this.author = author;
 		this.content = content;
+		this.time = time;
+	}
+	
+	public String getAuthor() {
+		return author;
 	}
 	
 	public String getContent() {
 		return content;
+	}
+	
+	public String getTime() {
+		return time;
 	}
 }
