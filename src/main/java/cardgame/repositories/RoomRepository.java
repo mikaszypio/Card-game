64ed31eb658292ae16807d7987299316de7069a1
mikaszypio <szypiorowski.m@gmail.com>
2019-01-17 21:02:32 +0100

    Update repositories with new queries

--- src/main/java/cardgame/repositories/RoomRepository.java
+++ src/main/java/cardgame/repositories/RoomRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface RoomRepository extends CrudRepository<Room, Long> {
 	
 	Room findByActiveAndReadiness(boolean active, byte readiness);
+	Room findByName(String name);
 }
--- src/main/java/cardgame/repositories/UserRepository.java
+++ src/main/java/cardgame/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package cardgame.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import cardgame.model.User;
+import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 	
+	@Query("select u from User u where u.room.roomId = ?1")
+	List<User> findByRoomId(Long roomId);
 }
