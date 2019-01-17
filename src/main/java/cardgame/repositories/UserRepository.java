03f1246a073ff3f003c53266eb76b043752032e6
mikaszypio <szypiorowski.m@gmail.com>
2019-01-17 23:27:58 +0100

    Organize and clean up role and user repositories

--- src/main/java/cardgame/repositories/UserRepository.java
+++ src/main/java/cardgame/repositories/UserRepository.java
@@ -1,13 +1,8 @@
 package cardgame.repositories;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import cardgame.model.User;
-import java.util.List;
+import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 	
-	@Query("select u from User u where u.room.roomId = ?1")
-	List<User> findByRoomId(Long roomId);
 }
