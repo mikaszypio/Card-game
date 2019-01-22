31009017e496323755915e1f7320a052b527496d
mikaszypio <szypiorowski.m@gmail.com>
2019-01-22 22:31:14 +0100

    Add login controller stub

---
+++ src/main/java/cardgame/controllers/LoginController.java
@@ -0,0 +1,18 @@
+package cardgame.controllers;
+
+import cardgame.model.User;
+import cardgame.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	public boolean CheckCredentials(User user) throws Exception {
+		User potentialUser = userRepository.findByUsername(user.getUsername());
+		return (potentialUser.getPassword().equals(user.getPassword()));
+	}
+}
