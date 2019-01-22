f23ab08141a74552e0472e163093952d1d70dbff
mikaszypio <szypiorowski.m@gmail.com>
2019-01-22 22:45:00 +0100

    Add room controller stub

---
+++ src/main/java/cardgame/controllers/RoomController.java
@@ -0,0 +1,20 @@
+package cardgame.controllers;
+
+import cardgame.model.Room;
+import cardgame.model.User;
+import cardgame.services.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RoomController {
+	
+	@Autowired
+	RoomService roomService;
+	
+	public void createRoom(String name, User user) {
+		
+		Room room = roomService.getEmptyRoom(name);
+		roomService.joinRoom(user, room);
+	}
+}
