32ebae6d72e04e2f589f95acc6d8a6eb178d5f3c
fleskiewicz <filipleskiewicz@gmail.com>
2019-01-24 09:02:25 +0100

    Add temporary users finding

--- src/main/java/cardgame/repositories/UserRepository.java
+++ src/main/java/cardgame/repositories/UserRepository.java
@@ -11,5 +11,8 @@ public interface UserRepository extends CrudRepository<User, Long> {
 	@Query("select u from User u where u.role.name = player")
 	public List<User> findAllPlayers(Sort sort);
 	
+	@Query("select u from User u where u.role.name = temporary")
+	public List<User> findTemporaryUsers(Sort sort);
+	
 	public User findByUsername(String username);
 }
