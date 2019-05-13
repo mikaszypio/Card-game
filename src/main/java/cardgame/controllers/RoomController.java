805e6d4e8adeeca7edb784e845368e710c433a5c
mikaszypio <szypiorowski.m@gmail.com>
2019-05-13 12:00:57 +0200

    Add join and leave room to room controller

--- src/main/java/cardgame/controllers/RoomController.java
+++ src/main/java/cardgame/controllers/RoomController.java
@@ -2,8 +2,12 @@ package cardgame.controllers;
 
 import cardgame.model.Room;
 import cardgame.model.User;
-import cardgame.services.RoomService;
+import cardgame.repositories.IRoomRepository;
+import cardgame.repositories.IUserRepository;
+import cardgame.services.IRoomService;
+import cardgame.services.IUserService;
 import java.util.List;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -11,7 +15,16 @@ import org.springframework.stereotype.Controller;
 public class RoomController {
 	
 	@Autowired
-	RoomService roomService;
+	IRoomRepository roomRepository;
+	
+	@Autowired
+	IRoomService roomService;
+	
+	@Autowired
+	IUserRepository userRepository;
+	
+	@Autowired
+	IUserService userService;
 	
 	public void createRoom(String name, User user) {
 		
@@ -21,5 +34,27 @@ public class RoomController {
 	
 	public List<Room> getRooms() {
 		return roomService.getActiveRooms();
+	}
+	
+	public void joinRoom(String roomName, HttpSession session) {
+		
+		User user;
+		Long userId = (Long) session.getAttribute("userId");
+		if(userId == null) {
+			user = userService.getUnregistered();
+			session.setAttribute("userId", Long.toString(user.getUserId()));
+		} else {
+			user = userRepository.findById(userId).get();
+		}
+		
+		Room room = roomRepository.findByName(roomName);
+		roomService.joinRoom(user, room);
+	}
+	
+	public void leaveRoom(HttpSession session) {
+		
+		Long userId = (Long) session.getAttribute("userId");
+		User user = userRepository.findById(userId).get();
+		roomService.leaveRoom(user);
 	}
 }
